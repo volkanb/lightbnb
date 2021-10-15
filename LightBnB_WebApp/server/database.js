d6c0ad19afa044548f2b0fe221f7da95932a0272
Volkan Benli <volkan.benli@lighthouselabs.com>
2021-10-15 07:48:22 -0400

    Converts addUser function to use pg for DB query

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -54,10 +54,17 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser =  function(user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  return pool
+    .query(`INSERT INTO users (name, email, password)
+      VALUES ($1, $2, 'password')
+      RETURNING *`,
+      [user.name, user.email])
+    .then((result) => {
+      return result.rows[0] || null;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 }
 exports.addUser = addUser;
 
