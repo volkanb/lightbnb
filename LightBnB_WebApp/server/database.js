a4f5ca8c86ccdfc75b8505cb5cccae7b1d663538
Volkan Benli <volkan.benli@lighthouselabs.com>
2021-10-15 08:11:15 -0400

    Convert getAllReservations function to use pg for DB query

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -76,7 +76,15 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  console.log(`I'M IN RESERVATIONS GUEST ID: ${guest_id}`);
+  return pool
+    .query(`SELECT * FROM reservations WHERE guest_id = $1 LIMIT $2`, [guest_id, limit])
+    .then((result) => {
+      return result.rows;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 }
 exports.getAllReservations = getAllReservations;
 
